18eee0dc0894171a392d293baaf854af1123e414
JINS-MACBOOK <soundb@192.168.2.2>
2017-03-20 20:43:28 +0900

    test

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ var userCnt = 0;
 var numUsers = 0;
 
 io.of('/lounge').on('connection', function(socket) {
+	
 	var addedUser = false;
 	
 	socket.on('lounge refresh', function(username) {
@@ -79,11 +80,11 @@ io.of('/lounge').on('connection', function(socket) {
 
 		var roomName = 'room-' + data.roomId;
 
-		socket.join(roomName);
+		socket.leave('lounge');
 
 		socket.emit('join room', data);
-
-		socket.broadcast.emit('room refresh', ':::: complete' + data.id);
+		
+		io.of('/waitroom').in(roomName).emit('room refresh', ':::: complete' + data.id);
 
 	});
 
@@ -108,59 +109,42 @@ io.of('/lounge').on('connection', function(socket) {
 
 	});
 
-	// when the client emits 'new message', this listens and executes
 	socket.on('new message', function(data) {
-		// we tell the client to execute 'new message'
-		socket.broadcast.emit('new message', {
+
+		socket.broadcast.to('lounge').emit('new message', {
 			username : socket.username,
 			message : data
 		});
 	});
 
-	// when the client emits 'add user', this listens and executes
-	socket.on('add user', function(username) {
-		// we store the username in the socket session for this client
-		socket.username = username;
-		// add the client's username to the global list
-		usernames[username] = username;
-		++numUsers;
-		addedUser = true;
-		socket.emit('login', {
-			numUsers : numUsers
-		});
-		// echo globally (all clients) that a person has connected
-		socket.broadcast.emit('user joined', {
-			username : socket.username,
-			numUsers : numUsers
-		});
-	});
-
-	// when the client emits 'typing', we broadcast it to others
-	socket.on('typing', function() {
-		socket.broadcast.emit('typing', {
-			username : socket.username
-		});
-	});
-
-	// when the client emits 'stop typing', we broadcast it to others
-	socket.on('stop typing', function() {
-		socket.broadcast.emit('stop typing', {
-			username : socket.username
-		});
-	});
-
-	// when the user disconnects.. perform this
 	socket.on('disconnect', function() {
-		// remove the username from global usernames list
+		
 		if (addedUser) {
 			delete usernames[socket.username];
 			--userCnt;
 
-			// echo globally that this client has left
 			socket.broadcast.emit('user left', {
 				username : socket.username,
 				numUsers : numUsers
 			});
 		}
 	});
+});
+
+io.of('/waitroom').on('connection', function(socket) {
+	
+	socket.on('waitroom refresh', function(data) {
+		
+		var roomName = 'room-' + data.roomId;
+
+		socket.emit('waitroom refresh', ':::waitroom refresh:::');
+
+		socket.broadcast.to('lounge').emit('waitroom refresh', loungeInfo);
+
+	});
+
+	socket.on('disconnect', function() {
+		
+	});
+	
 });
